5469d2c1577a485d5fa0e31db3c7d1d557b09c85
tony.chromebook <tonetheman@gmail.com>
2021-12-21 08:14:03 -0600

    re arranged to an OO thing easier to keep up with the data

--- make_test_ppm.cpp
+++ make_test_ppm.cpp
@@ -35,34 +35,42 @@ void print_pixel(Pixel& p) {
     printf("%d %d %d\n", p.r, p.g, p.b);
 }
 
-void get(unsigned char * data, int x, int y, int n, 
-    int X, int Y, Pixel *pixel) {
-    int index = (Y*y*3) + (X*x*3);
-    pixel->r = data[index];
-    pixel->g = data[index+1];
-    pixel->b = data[index+2];
-}
 
-
-int main() {
-
+struct Image {
     // x is width
     // y is height
     // n is number of color channels
     // all of ^ is from docs on stb
+    unsigned char * data;
     int x;
     int y;
     int n;
-    unsigned char * data = stbi_load("testimage.png", &x, &y, &n, 0);
 
-    Pixel p;
-    for(int i=0;i<32;i++) {
-        get(data,x,y,n,i,0,&p);
-        print_pixel(p);
+    Image(const char * filename) {
+        this->data = stbi_load(filename, 
+            &(this->x), &(this->y), &(this->n), 0);
+
     }
 
-    get(data,x,y,n,0,1,&p);
-    print_pixel(p);
+    void get(int X, int Y, Pixel *pixel) {
+        int index = (Y*this->y*3) + (X*this->x*3);
+        pixel->r = data[index];
+        pixel->g = data[index+1];
+        pixel->b = data[index+2];
+    }
+
+
+};
+
+int main() {
+
+    Image img("testimage.png");
+    
+    Pixel p;
+    for(int i=0;i<32;i++) {
+        img.get(i,0,&p);
+        print_pixel(p);
+    }
 
     return 0;
 }
