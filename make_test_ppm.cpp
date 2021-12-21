b72c1cbda1e3a0c90ef097709de2de1feb747b66
Tony Colston <tonetheman@gmail.com>
2021-12-21 13:38:23 -0600

    saving work added a write without a separate call out to convert

--- make_test_ppm.cpp
+++ make_test_ppm.cpp
@@ -2,6 +2,8 @@
 #include <cstdio>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 // make a simple test image
 // with known values
@@ -11,19 +13,30 @@ void create_test_image() {
     fprintf(outf,"#tony is cool\n");
 
     int width = 32;
-    int height = 32;
+    int height = 4;
     fprintf(outf,"%d %d\n",width,height);
     fprintf(outf, "255\n");
 
-    for (int i=0;i<width;i++) {
-        for (int j=0;j<height;j++) {
-            fprintf(outf, "%d %d %d ", i, j, 5);
+    for(int j=0;j<4;j++) {
+        for (int i=0;i<width;i++) {
+            fprintf(outf, "%d %d %d ", i, j, ((i*j)%255));
         }
         fprintf(outf,"\n");
     }
 
     fclose(outf);
 }
+
+// call out to /usr/local/bin/convert
+// to convert the image from ppm to png
+void convert(const char *in_filename, const char *out_filename) {
+    char buffer[512];
+    memset(buffer,'\0',512);
+    sprintf(buffer,"/usr/local/bin/convert %s %s",in_filename, out_filename);
+    int res = system(buffer);
+    printf("res: from convert: %d\n", res);
+}
+
 
 struct Pixel {
     int r;
@@ -42,35 +55,64 @@ struct Image {
     // n is number of color channels
     // all of ^ is from docs on stb
     unsigned char * data;
-    int x;
-    int y;
-    int n;
+    int width;
+    int height;
+    int channels;
 
     Image(const char * filename) {
+        printf("before loading image..\n");
         this->data = stbi_load(filename, 
-            &(this->x), &(this->y), &(this->n), 0);
-
+            &(this->width), &(this->height), &(this->channels), 0);
+        printf("done loading image\n");
+        printf("x y n: %d %d %d\n",width,height,channels);
     }
 
-    void get(int X, int Y, Pixel *pixel) {
-        int index = (Y*this->y*3) + (X*this->x*3);
+    void get(int x, int y, Pixel *pixel) {
+        int index = (y*this->height*3) + (x*this->width*3);
         pixel->r = data[index];
         pixel->g = data[index+1];
         pixel->b = data[index+2];
     }
 
-
 };
+
+void reading() {
+    Image img("testimage.png");
+    for(int i=0;i<32*4*3;i++) {
+        printf("%d\n",img.data[i]);
+    }
+}
+
+void writing() {
+    char buffer[32*4*3];
+    int pos = 0;
+    for(int j=0;j<4;j++) {
+        for (int i=0;i<32;i++) {
+            buffer[pos++] = i;
+            buffer[pos++] = j;
+            buffer[pos++] = (i*j)%255;
+        }
+    }
+
+    int res = stbi_write_png("out.png",
+        32,4, // width and height
+        3, // channels
+        buffer,
+        32*3); // stride, distance between rows
+    // res == 0 is FAILURE
+    // anything else is SUCCESS
+    
+    printf("res from write: %d\n",res);
+}
 
 int main() {
 
-    Image img("testimage.png");
-    
-    Pixel p;
-    for(int i=0;i<32;i++) {
-        img.get(i,0,&p);
-        print_pixel(p);
-    }
+    // create_test_image();
+    // convert("testimage.ppm","testimage.png");
+
+    // reading();
+
+    // writing();
 
     return 0;
 }
