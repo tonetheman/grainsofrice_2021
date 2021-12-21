6bcda70f2efa03f3651990367973b16b127676ca
tony(kitchen) <tonetheman@gmail.com>
2021-12-20 18:33:25 -0600

    saving the get not sure if it is right or not yet

--- mainline.cpp
+++ mainline.cpp
@@ -3,6 +3,23 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+struct Pixel {
+    int r;
+    int g;
+    int b;
+};
+
+void print_pixel(Pixel* p) {
+    printf("%d %d %d\n", p->r, p->g, p->b);
+}
+
+void get(unsigned char * data, int x, int y, int n, 
+    int X, int Y, Pixel *pixel) {
+    int index = (Y*y*3) + (X*x*3);
+    pixel->r = data[index];
+    pixel->g = data[index+1];
+    pixel->b = data[index+2];
+}
 
 // will write a filename called crud.ppm
 // that is a P3 type of ppm file
@@ -29,6 +46,8 @@ void verify(unsigned char * data, int x, int y, int n) {
     fclose(outf);
 }
 
+// will only write the red channel to the ppm
+// effectively making it grayscale
 void verify_red(unsigned char * data, int x, int y, int n) {
     FILE* outf = fopen("crud.ppm","w");
     fprintf(outf, "P3\n# tony is cool\n");
@@ -89,14 +108,17 @@ int main() {
     // all of ^ is from docs on stb
 
     // write data out to ppm for debugging only
-    //verify(data,x,y,n);
+    verify(data,x,y,n);
     //verify_red(data,x,y,n);
 
     // do something cool here
     // ???
-    threshold(data,x,y,n);
+    //threshold(data,x,y,n);
 
-
+    Pixel p;
+    get(data,x,y,n,
+        0,1,&p);
+    print_pixel(&p);
     
     return 0;
 }
